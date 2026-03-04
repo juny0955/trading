a8a787cfcb9d8d5c8a241e26d57da80408676696
Choi Junyoung <juny0955@gmail.com>
2026-03-04 14:36:00 +0900

    feat: cancelOrder 202 변경

--- src/main/java/dev/junyoung/trading/order/adapter/in/rest/OrderController.java
+++ src/main/java/dev/junyoung/trading/order/adapter/in/rest/OrderController.java
@@ -42,7 +42,7 @@ public class OrderController {
         cancelOrderUseCase.cancelOrder(orderId);
 
         return ResponseEntity
-                .ok()
+                .accepted()
                 .build();
     }
 

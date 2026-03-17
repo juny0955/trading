dae3189b4cee0547f14c07c423b3d01f24681377
Choi Junyoung <juny0955@gmail.com>
2026-03-17 22:50:07 +0900

    refactor: for 루프 -> stream api 변경

--- src/main/java/dev/junyoung/trading/order/domain/service/dto/OrderIndex.java
+++ src/main/java/dev/junyoung/trading/order/domain/service/dto/OrderIndex.java
@@ -1,21 +1,18 @@
 package dev.junyoung.trading.order.domain.service.dto;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import dev.junyoung.trading.common.exception.BusinessRuleException;
 import dev.junyoung.trading.order.domain.model.entity.Order;
 import dev.junyoung.trading.order.domain.model.value.OrderId;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public record OrderIndex (Map<OrderId, Order> orderIndex) {
     public static OrderIndex of(List<Order> orders) {
-        Map<OrderId, Order> orderMap = new HashMap<>();
-        for (Order order : orders) {
-            orderMap.put(order.getOrderId(), order);
-        }
-
-        return new OrderIndex(orderMap);
+        return new OrderIndex(orders.stream()
+            .collect(Collectors.toMap(Order::getOrderId, Function.identity())));
     }
 
     public Order findOrder(OrderId orderId) {
